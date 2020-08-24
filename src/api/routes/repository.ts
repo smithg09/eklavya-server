478008cde85226b6dea9cec1b41e13f8da87e317
smithg09 <smith.gajjar@gmail.com>
2020-08-24 14:12:13 +0530

    Single Repo Data Route

--- src/api/routes/repository.ts
+++ src/api/routes/repository.ts
@@ -9,7 +9,7 @@ import { Logger } from 'winston';
 const route = Router();
 
 export default (app: Router) => {
-  app.use('/repositories', route);
+  app.use('/repositoryData', route);
 
   route.get('/all', async (req: Request, res: Response, next: NextFunction) => {
     const logger: Logger = Container.get('logger');
@@ -19,6 +19,20 @@ export default (app: Router) => {
       const repositoryData = await repositoryModel.find({});
 
       res.status(200).json(repositoryData);
+    } catch (e) {
+      logger.error('🔥 error: %o', e);
+      return next(e);
+    }
+  });
+
+  route.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    const logger: Logger = Container.get('logger');
+    logger.debug('Getting %o data', req.params.id);
+    try {
+      const repositoryModel = Container.get('repositoryModel') as mongoose.Model<IRepository & mongoose.Document>;
+      const data = await repositoryModel.findById(req.params.id);
+
+      res.status(200).json(data);
     } catch (e) {
       logger.error('🔥 error: %o', e);
       return next(e);
