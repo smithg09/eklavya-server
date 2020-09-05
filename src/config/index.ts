fb5761e62d27c529e9de56f85ca96600d3dbd2af
smithg09 <smith.gajjar@gmail.com>
2020-09-05 15:43:52 +0530

    Code cleanup

--- src/config/index.ts
+++ src/config/index.ts
@@ -53,13 +53,6 @@ export default {
     prefix: '/api',
   },
   /**
-   * Mailgun email credentials
-   */
-  emails: {
-    apiKey: process.env.MAILGUN_API_KEY,
-    domain: process.env.MAILGUN_DOMAIN,
-  },
-  /**
    * Google Login OAuth Credentials
    */
   OAuth2: {
